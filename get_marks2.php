0660fa4013315e938065ebd80e7f292d93f3c3c6
Krishanu Deb <kdkrishanu.deb@gmail.com>
2021-03-14 20:04:16 +0530

    view marks 95% done

--- get_marks2.php
+++ get_marks2.php
@@ -3,13 +3,17 @@
 include 'dbcon.php';
 include 'functions.inc.php'; //contains imp functions
 
+$total=0;
+$count=0;
+
+$student_id=get_safe_value($con,$_POST['student_id']);
+
 $class_id=get_safe_value($con,$_POST['class_id']);
 
 
-$sql2="select student.name as sname,student.roll_no,subject.name as subname,marks.marks from student,subject,marks where student.id = marks.student_id and student.class_id= '$class_id' and marks.sub_id=subject.id order by student.roll_no  ";
+$sql2="select  distinct(student.name) as sname, marks.marks, subject.name as subname from student,marks,subject where student.id = '$student_id' and marks.student_id = '$student_id' and student.class_id ='$class_id' and subject.class_id= '$class_id' and marks.sub_id = subject.id";
 
  $res2=mysqli_query($con,$sql2);
-
 if(mysqli_num_rows($res2)>0)
 {
     $html="
@@ -19,7 +23,7 @@ if(mysqli_num_rows($res2)>0)
         <thead>
 
         <th>Name</th>
-        <th>Roll no</th>
+        
         <th>Subject Name</th>
         <th>Marks</th>
 
@@ -41,15 +45,20 @@ if(mysqli_num_rows($res2)>0)
                                             {     
                                                        $html.= "<tr>";
                                                $html.="<td>".$row2['sname']."</td>";
-                                               $html.="<td>".$row2['roll_no']."</td>";
+                                               
                                                $html.="<td>".$row2['subname']."</td>";
                                                $html.="<td>".$row2['marks']."</td>";
 
                                                $html.= "</tr>";
-
+                                                   $total=$total + $row2['marks'];
+                                                   $count++;
                                             }
 
+                                            $totalmarks = $count * 100;
+                                            $percentage = ( $total / $totalmarks ) * 100;
 
+                                                 $html.="<tr><td colspan='3'><span > &ensp; Total:=".$total." out of ".$totalmarks."</span></td></tr>
+                                                 <tr><td colspan='3'><span > &ensp; percentage:=".$percentage." %</span></td></tr>";
           
 
 
@@ -61,6 +70,10 @@ else
 {
    echo "<h2>No DATA available for now</h2>";
 }
+
+
+
+
 ?>
 
 
--- view_marks.php
+++ view_marks.php
@@ -144,6 +144,25 @@ $msg='';
             </div> <!-- /widget-header -->
           
           <div class="widget-content">
+
+
+            <div class="tabbable">
+            <ul class="nav nav-tabs">
+              <li>
+                <a href="#Class_Wise" data-toggle="tab">Class Wise</a>
+              </li>
+              <li  class="active"><a href="#jscontrols" data-toggle="tab">Student Wise</a></li>
+
+              </li>
+              <li  ><a href="#rank" data-toggle="tab">Rank</a></li>
+              <li  ><a href="#analysis" data-toggle="tab">analysis</a></li>
+            </ul>
+            
+            <br>
+            
+              <div class="tab-content">
+                <div class="tab-pane" id="Class_Wise">
+                
             
             <div class="control-group">                     
                       <select name="class_id" id="class_id" onclick="get_marks('')" required>
@@ -244,7 +263,267 @@ $msg='';
       </table>-->
     </div>
   </body>
+          </div>
+                
+                <div class="tab-pane active" id="jscontrols">
+
+                  <div class="control-group">                     
+                      <select name="class_id2" id="class_id2" onchange="get_rollno('')" required>
+    <option value="">select class
+                              </option>
+                              <?php
+
+                              $res2 = mysqli_query($con,"select * from class order by name asc");
+                             while($row2=mysqli_fetch_assoc($res2))
+
+                             {
+                              if($row['id']==$class_id2)
+                              {
+                                 echo "<option  selected value =".$row2['id'].">".$row2['name']."</option>";
+
+                              }
+                              else
+                              {
+                                 echo "<option value =".$row2['id'].">".$row2['name']."</option>";
+
+                              }
+                             }
+                              
+                              
+                              ?>
+
+
+                               </select>      
+                    </div> <!-- /control-group -->
+
+                    <div class="control-group">                     
+                      <label class="control-label" for="student_id">Roll no</label>
+                      <div class="controls">
+                       <div class="form-group">
+                              
+                              <select class=" form-control" name="student_id" id="student_id" onclick="get_marks2('')" required>
+                              <option>select Roll no
+                              </option>
+                              
+
+
+                               </select> </div>
+                      </div> <!-- /controls -->       
+                    </div> <!-- /control-group -->
             
+            
+                <body>
+    <div class="container3" id="table2" name="table2">
+      <h2>please select class and Roll no to get marks</h2>
+      <!--<table>
+        <thead>
+          <tr >
+
+            
+            
+          <tr>  
+        </thead>
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td>Ali</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>02</td>
+            <td>Salman</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>03</td>
+            <td>Shan</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>04</td>
+            <td>Aliyan</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>05</td>
+            <td>Zeeshan</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+        </tbody>
+        <!-- footer 
+        <tfoot>
+          <tr>
+            <td colspan=3>Maximum Marks: </td>
+            <td colspan=3>Marks Obtained: </td>
+            <td colspan=2>Grade: </td>
+          </tr>
+        </tfoot> footer ends
+      </table>-->
+    </div>
+  </body>
+
+
+                </div>  
+
+
+                <div class="tab-pane" id="rank">
+
+                  <div class="control-group">                     
+                      <select name="class_id3" id="class_id3"  required>
+    <option value="">select class
+                              </option>
+                              <?php
+
+                              $res2 = mysqli_query($con,"select * from class order by name asc");
+                             while($row2=mysqli_fetch_assoc($res2))
+
+                             {
+                              if($row['id']==$class_id2)
+                              {
+                                 echo "<option  selected value =".$row2['id'].">".$row2['name']."</option>";
+
+                              }
+                              else
+                              {
+                                 echo "<option value =".$row2['id'].">".$row2['name']."</option>";
+
+                              }
+                             }
+                              
+                              
+                              ?>
+
+
+                               </select>      
+                    </div> <!-- /control-group -->
+
+                    <div class="control-group">                     
+                      <label class="control-label" for="student_id">Term</label>
+                      <div class="controls">
+                       <div class="form-group">
+                              
+                              <select name="term_id" id="term_id" required onclick="get_rank('')" >
+    <option value="">select term
+                              </option>
+                              <?php
+
+                              $res = mysqli_query($con,"select * from term");
+                             while($row=mysqli_fetch_assoc($res))
+
+                             {
+                              if($row['id']==$term_id)
+                              {
+                                 echo "<option  selected value =".$row['id'].">".$row['name']."</option>";
+
+                              }
+                              else
+                              {
+                                 echo "<option value =".$row['id'].">".$row['name']."</option>";
+
+                              }
+                             }
+                              
+                              
+                              ?>
+
+
+                               </select>   </div>
+                      </div> <!-- /controls -->       
+                    </div> <!-- /control-group -->
+            
+            
+                <body>
+    <div class="container4" id="table3" name="table3">
+      <h2>please select class and Roll no to get marks</h2>
+      <!--<table>
+        <thead>
+          <tr >
+
+            
+            
+          <tr>  
+        </thead>
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td>Ali</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>02</td>
+            <td>Salman</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>03</td>
+            <td>Shan</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>04</td>
+            <td>Aliyan</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+          <tr>
+            <td>05</td>
+            <td>Zeeshan</td>
+            <td>86</td>
+            <td>77</td>
+            <td>87</td>
+            <td>92</td>
+            <td>95</td>
+          </tr>
+        </tbody>
+        <!-- footer 
+        <tfoot>
+          <tr>
+            <td colspan=3>Maximum Marks: </td>
+            <td colspan=3>Marks Obtained: </td>
+            <td colspan=2>Grade: </td>
+          </tr>
+        </tfoot> footer ends
+      </table>-->
+    </div>
+  </body>
+
+
+                </div>  
             
             
           </div> <!-- /widget-content -->
@@ -364,6 +643,60 @@ $msg='';
          data:'class_id='+class_id,
          success:function(result) {
             jQuery('#table').html(result);
+         }
+
+
+      });
+   }
+
+   function get_rollno(stud_id)
+   {
+
+
+      var class_id2=jQuery('#class_id2').val();
+      jQuery.ajax({
+
+         url:'get_rollno.php',
+         type:'post',
+         data:'class_id='+class_id2+'&student_id='+student_id,
+         success:function(result) {
+            jQuery('#student_id').html(result);
+         }
+
+
+      });
+   }
+
+   function get_marks2(stud_id)
+   {
+     
+      var class_id=jQuery('#class_id2').val();
+      var student_id=jQuery('#student_id').val();
+      jQuery.ajax({
+
+         url:'get_marks2.php',
+         type:'post',
+         data:'student_id='+student_id+'&class_id='+class_id,
+         success:function(result) {
+            jQuery('#table2').html(result);
+         }
+
+
+      });
+   }
+
+   function get_rank(stud_id)
+   {
+     
+      var term_id=jQuery('#term_id').val();
+      var class_id=jQuery('#class_id3').val();
+      jQuery.ajax({
+
+         url:'get_rank.php',
+         type:'post',
+         data:'class_id='+class_id+'&term_id='+term_id,
+         success:function(result) {
+            jQuery('#table3').html(result);
          }
 
 
